470db280459d16897ef61ead38e6e202b8482a25
nietiene <niyomugaboetiene53@gmail.com>
2025-08-06 08:29:12 -0700

    first commit of youtube video downlloader

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
-import exress from "express"
+import express from "express"
 import cors from "cors"
 import ytdl from "ytdl-core"
 
-const app = export()
+const app = express()
+
+app.get('/download', async (req, res) => {
+    const videoURL = req.query.url;
+
+    if (!ytdl.validateURL(videoURL)) {
+        return
+    }
+})
