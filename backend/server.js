2d89a1ed75f4ffdd4c01621001c301cbf255763d
nietiene <niyomugaboetiene53@gmail.com>
2025-08-06 08:34:12 -0700

    first commit of youtube video downlloader

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,20 @@ app.get('/download', async (req, res) => {
     const videoURL = req.query.url;
 
     if (!ytdl.validateURL(videoURL)) {
-        return
+        return res.status(400).json({ error: "Invalid Youtube URL" })
     }
+
+    const videoID = ytdl.getURLVideoID(videoURL)
+    const info = ytdl.getInfo(videoID)
+    const title = (await info).videoDetails.title
+
+    res.header("Content-Disposition", `attachment; filename=${title}.mp4`)
+
+    ytdl(videoURL, {
+        format: "mp4",
+        quality: "highestvideo",
+    }).pipe(res)
 })
+
+const PORT = 500
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
