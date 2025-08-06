789c4c8959975e3d7b1b164f4ae5f0c2c6ac659f
nietiene <niyomugaboetiene53@gmail.com>
2025-08-06 09:12:38 -0700

    first commit of youtube video downlloader

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,29 @@ import express from "express"
 import cors from "cors"
 import ytdl from "ytdl-core"
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.post('/download', async (req, res) => {
-    const videoURL = req.body.url
+    const videoURL = req.body.url;
 
     if (!ytdl.validateURL(videoURL)) {
-        return res.status(400).json({ error: "Invalid YouTube URL" })
+        return res.status(400).json({ error: "Invalid YouTube URL" });
     }
 
     try {
-        const info = await ytdl.getInfo(videoURL)
-        const title = info.videoDetails.title.replace(/[^\w\s]/gi, "")
-        res.header("Content-Disposition", `attachment; filename="${title}.mp4"`)
+        const info = await ytdl.getInfo(videoURL);
+        const title = info.videoDetails.title.replace(/[^\w\s]/gi, "");
+        res.header("Content-Disposition", `attachment; filename="${title}.mp4"`);
 
-        ytdl(videoURL, { quality: "highestvideo"}).pipe(res);
+        ytdl(videoURL, { quality: "highest", filter: "audioandvideo"}).pipe(res);
     } catch (err) {
-        res.status(500).json({ error: "Failed to donwload video "})
+        console.error(err);
+        res.status(500).json({ error: "Failed to donwload video "});
     }
 })
 
 const PORT = 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
